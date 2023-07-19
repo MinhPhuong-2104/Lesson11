b43687afb8504e1deeeeb695a9da128c9c87d28a
MinhPhuong-2104 <116358834+MinhPhuong-2104@users.noreply.github.com>
2023-07-19 17:21:04 +0700

    Update delete.php

---
+++ category/delete.php
@@ -0,0 +1,7 @@
+<?php
+    require_once 'pdo.php';
+    $cate = new Category();
+    $id = ['id' => $_POST['id']];
+    $cate->deleteData($id);
+    header("Location: http://localhost/learn_php/BTVN11/category/index.php");
+?>
